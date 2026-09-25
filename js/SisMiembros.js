b23b0d68b494b8ad492346df9e60cbbf464852a0
GamerCB Oficial <deibyrt107cali@gmail.com>
2026-09-25 14:53:17 -0500

    Delete js/SisMiembros.js

--- js/SisMiembros.js
+++
@@ -1,6 +0,0 @@
-/** Compat: carga el gate de clave (js/membresia.js). */
-(function () {
-  if (typeof window.iniciarSistemaMembresia === "function") {
-    window.iniciarSistemaMembresia();
-  }
-})();
